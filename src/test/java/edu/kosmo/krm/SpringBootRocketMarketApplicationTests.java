dd2990da93dc271908d6b8c9256f7a416180fcfb
DESKTOP-54O77T3\samsung <pso1030@naver.com>
2022-02-22 16:29:40 +0900

    start project! first commit! 기본 설정 후 프로젝트 생성

---
+++ src/test/java/edu/kosmo/krm/SpringBootRocketMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.kosmo.krm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRocketMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
