22bd60735c516353a2b306e03561156cece57355
pso1030@naver.com <pso1030@naver.com>
2022-03-19 23:08:50 +0900

    ReviewService.java파일이 저장 안되던 오류 해결

--- src/main/java/edu/kosmo/krm/service/ReviewService.java
+++ src/main/java/edu/kosmo/krm/service/ReviewService.java
@@ -1,18 +1,19 @@
 package edu.kosmo.krm.service;
-// 리뷰 관련 서비스
+
 
 import java.util.List;
-
-import org.springframework.stereotype.Service;
 
 import edu.kosmo.krm.page.Criteria;
 import edu.kosmo.krm.joinVO.JoinReviewVO;
 import edu.kosmo.krm.vo.MemberVO;
 
-@Service
 public interface ReviewService {
 	
-	// 내가 쓴 리뷰 내역 가져오는 코드
+	// 내가 쓴 후기 내역 가져오는 코드
 	public List<JoinReviewVO> review_getList(Criteria criteria, MemberVO memberVo);
+		
 	public int review_getTotal(MemberVO memberVO);
+
+
+
 }
